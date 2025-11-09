69dd1aab018d25a6e486a788cbd4d6ae69a95693
David Hochhauser <daveh@csd.net>
2025-11-09 09:44:49 -0700

    .

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,7 +4,7 @@
 # ln -s "/Applications/NetBeans/Apache NetBeans 12.1.app/Contents/Resources/NetBeans/netbeans/bin/netbeans" /usr/local/bin/netbeans
 alias cypress='./node_modules/cypress/bin/cypress open --env type=dev --config baseUrl=http://localhost:8080 &'
 alias diff-pdf='diff-pdf --view --mark-differences'
-alias lfcd='source lfcdscript' # this is required so that the cd command doesn't run in a subshell
+
 alias ll='ls -lhFG'
 alias lla='ls -alhFG'
 alias ls='ls -FG'
@@ -12,5 +12,6 @@ alias ls='ls -FG'
 alias tf='terraform'
 alias ql='qlmanage -p "$@" > /dev/null'
 alias vm='versionmanager'
+alias htop='btop'
 
 # see zsh_local.zsh for more aliases
